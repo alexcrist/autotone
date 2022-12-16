2913a5679c071d3e2ef07274ef2f158011cb75e2
alexcrist <alexecrist@gmail.com>
2022-12-16 14:30:08 -0800

    Finish Reaper C API, call from Python

--- reaper/reaper_c_api.cc
+++ reaper/reaper_c_api.cc
@@ -9,11 +9,93 @@
 #include "epoch_tracker/epoch_tracker.h"
 #include "wave/wave.h"
 
-// https://stackoverflow.com/questions/1615813/how-to-use-c-classes-with-ctypes
-
 extern "C" {
 
-  int helloworld() {
-    return 42;
+  EpochTracker * et;
+  Track * f0_track;
+  bool error;
+  
+  float sample_rate_;
+  bool do_highpass_; 
+  bool do_hilbert_transform_;
+
+  void init(
+    float sample_rate,
+    bool do_highpass, 
+    bool do_hilbert_transform
+  ) {
+    et = new EpochTracker();
+    sample_rate_ = sample_rate;
+    do_highpass_ = do_highpass; 
+    do_hilbert_transform_ = do_hilbert_transform;
+  }
+
+  void process(
+    int16_t * input, 
+    int32_t input_length
+  ) {
+    error = false;
+
+    // Calculate frequencies
+    if (!et->Init(
+      input,
+      input_length,
+      sample_rate_,
+      kMinF0Search,
+      kMaxF0Search,
+      do_highpass_,
+      do_hilbert_transform_
+    )) {
+      fprintf(stderr, "Failed to run EpochTracker.Init()\n");
+      error = true;
+      return;
+    }
+    if (!et->ComputeFeatures()) {
+      fprintf(stderr, "Failed to run EpochTracker.ComputeFeatures()\n");
+      error = true;
+      return;
+    }
+    if (!et->TrackEpochs()) {
+      fprintf(stderr, "Failed to run EpochTracker.TrackEpochs()\n");
+      error = true;
+      return;
+    }
+    std::vector<float> f0;
+    std::vector<float> corr;
+    if (!et->ResampleAndReturnResults(kExternalFrameInterval, &f0, &corr)) {
+      fprintf(stderr, "Failed to run EpochTracker.ResampleAndReturnResults()\n");
+      error = true;
+      return;
+    }
+
+    delete f0_track;
+    f0_track = new Track();
+    f0_track->resize(f0.size());
+    for (int32_t i = 0; i < f0.size(); ++i) {
+      float t = kExternalFrameInterval * i;
+      f0_track->t(i) = t;
+      f0_track->set_v(i, (f0[i] > 0.0) ? true : false);
+      f0_track->a(i) = (f0[i] > 0.0) ? f0[i] : -1.0;
+    }
+  }
+
+  bool get_output_error() {
+    return error;
+  }
+
+  int32_t get_output_length() {
+    return f0_track->num_frames();
+  }
+
+  void get_output(float * times, float * freqs) {
+    for (int32_t i = 0; i < f0_track->num_frames(); i++) {
+      times[i] = f0_track->t(i);
+      freqs[i] = f0_track->a(i);
+    }
+  }
+
+  void cleanup() {
+    delete et;
+    delete f0_track;
   }
 }
--- scripts/test_reaper.sh
+++ scripts/test_reaper.sh
@@ -1,7 +1,7 @@
 cd build;
 rm -rf output;
 mkdir output;
-./reaper -i ../audio/evil_test_laugh.wav -f output/bla.f0 -a;
+./reaper -i ../audio/evil_test_laugh.wav -f output/bla.f0 -p output/bla.pm -a;
 echo "File written to output/bla.f0";
-less output/bla.f0;
+echo "File written to output/bla.pm";
 cd ..;
